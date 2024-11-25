5d8dc2d18802f759704608848aa3628d88f0b57a
Will Berg <willberg510@gmail.com>
2024-11-25 18:29:34 -0500

    Initial commit with simple React app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
+const FetchGetRequest = () => {
+  const [data, setData] = useState(null);
+
+  const API_URL = 'https://dev.pulsoid.net/api/v1/data/heart_rate/latest';
+
+  useEffect(() => {
+    const fetchItems = async function() {
+      try {
+        const response = await fetch(API_URL, {
+          headers: {
+            "Authorization": "Bearer a051ca5c-1be4-4c94-91d1-c23937388f5c",
+          }
+        });
+        if (!response.ok) throw Error('Did not receive expected data');
+        const apiData = await response.json();
+        setData(apiData.data.heart_rate);
+        console.log(apiData);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchItems();
+  })
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b>{data}</b>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
+  )
+}
+
+function App() {
+  
+  return (
+    <FetchGetRequest></FetchGetRequest>
   );
 }
 
