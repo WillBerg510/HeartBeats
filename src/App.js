004206d8275f56b6acb256212d30741982666efc
Will Berg <willberg510@gmail.com>
2024-12-02 10:38:03 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -2,22 +2,63 @@ import logo from './logo.svg';
 import {useState, useEffect} from 'react';
 import './App.css';
 
+const generateRandomString = (length) => {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const values = crypto.getRandomValues(new Uint8Array(length));
+  return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+}
+
+const codeVerifier  = generateRandomString(64);
+
+const sha256 = async (plain) => {
+  const encoder = new TextEncoder()
+  const data = encoder.encode(plain)
+  return window.crypto.subtle.digest('SHA-256', data)
+}
+
+const base64encode = (input) => {
+  return btoa(String.fromCharCode(...new Uint8Array(input)))
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+}
+
+const hashed = await sha256(codeVerifier)
+const codeChallenge = base64encode(hashed);
+
+const clientId = '9da39ee411dc40c1b069995782a00621';
+const redirectUri = 'https://dsaproject3heartbeats.netlify.app/';
+
+const scope = 'user-read-private user-read-email';
+const authUrl = new URL("https://accounts.spotify.com/authorize")
+
+// generated in the previous step
+window.localStorage.setItem('code_verifier', codeVerifier);
+
+const params =  {
+  response_type: 'code',
+  client_id: clientId,
+  scope,
+  code_challenge_method: 'S256',
+  code_challenge: codeChallenge,
+  redirect_uri: redirectUri,
+}
+
+//authUrl.search = new URLSearchParams(params).toString();
+//window.location.href = authUrl.toString();
+
 const FetchGetRequest = () => {
   const [data, setData] = useState(null);
 
-  const API_URL = 'https://dev.pulsoid.net/api/v1/data/heart_rate/latest';
+  const API_URL = 'https://api.getsongbpm.com/search/?api_key=YOUR_API_KEY_HERE&type=artist&lookup=green+day';
 
   useEffect(() => {
     const fetchItems = async function() {
       try {
-        const response = await fetch(API_URL, {
-          headers: {
-            "Authorization": "Bearer a051ca5c-1be4-4c94-91d1-c23937388f5c",
-          }
-        });
+        const response = await fetch(API_URL);
         if (!response.ok) throw Error('Did not receive expected data');
         const apiData = await response.json();
-        setData(apiData.data.heart_rate);
+        setData(apiData);
         console.log(apiData);
       } catch (error) {
         console.log(error);
@@ -32,7 +73,7 @@ const FetchGetRequest = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <b>{data}</b>
+          <b>{data?.bpm}</b>
         </p>
       </header>
     </div>
@@ -40,7 +81,6 @@ const FetchGetRequest = () => {
 }
 
 function App() {
-  
   return (
     <FetchGetRequest></FetchGetRequest>
   );
